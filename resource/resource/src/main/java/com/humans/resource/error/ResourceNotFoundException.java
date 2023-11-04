5223922e1d06fca0dde5fa17104212656e96603c
OverDev <overman.zuley1234@gmail.com>
2023-11-03 21:36:49 -0600

    Eliminando el BackEnd

--- resource/resource/src/main/java/com/humans/resource/error/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.humans.resource.error;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String s) {
-        super(s);
-    }
-}
--- resource/resource/src/main/java/com/humans/resource/repository/PerfilamientoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.humans.resource.repository;
-
-import com.humans.resource.entity.Perfilamiento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerfilamientoRepository extends JpaRepository<Perfilamiento, Long> {
-}
