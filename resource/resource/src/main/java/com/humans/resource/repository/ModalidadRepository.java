80e72d5bb4ad5ad793cd2308e03a0e220de8caa0
OverDev <overman.zuley1234@gmail.com>
2023-11-07 16:39:44 -0600

    Eliminando version anterior

--- resource/resource/src/main/java/com/humans/resource/repository/ModalidadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.humans.resource.repository;
-
-import com.humans.resource.entity.Modalidad;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ModalidadRepository extends JpaRepository<Modalidad, Long> {
-}
